7605d5cdbcfee34ba3256ba12424ddb7713a7c9a
KodewithKing <87894543+KodewithKing@users.noreply.github.com>
2021-07-24 14:52:10 +0530

    Create main.java
    Adding  the main code

---
+++ main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+// we have imported a java package named Scanner.
+public class Main
+
+{
+
+	public static void main(String[] args)	{
+
+		Scanner input = new Scanner(System.in);
+//.  made a scanner
+		int i = 0;
+// initialisin i with 0
+		System.out.print("Enter a number: ");
+// takeing input from the user.
+		int tab = input.nextInt();
+// storing the input in a varible named tab
+	    
+
+		while(i <= 9) {
+// starting a while loop.
+// running the while until i <=9.
+			 i++;
+// i++ means adding 1 to i every time the loop runs.      
+
+			System.out.println(i * tab);
+// printing the value i x tab.
+		}
+
+	}
+
+}
